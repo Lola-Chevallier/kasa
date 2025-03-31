2b19987b91246ca2d3bdcbca2771a92f2836ca80
Lola Chevallier <lolachevallier@laposte.net>
2025-03-31 18:29:42 +0200

    style: implement responsive design for Home page (header, banner, cards, footer)

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import './styles/index.scss'
@@ -14,6 +15,7 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer/>
     </div>
   );
 }
---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "../styles/Footer.module.scss";
+import mobileFooter from "../assets/images/footer-mobile.png";
+import desktopFooter from "../assets/images/footer-desktop.png";
+
+const Footer = () => { 
+    return (
+        <footer className={styles.footer}>
+            <img className={styles.mobile} src={mobileFooter} alt="footer" />
+            <img className={styles.desktop} src={desktopFooter} alt="footer"/>
+        </footer>
+    );
+};
+
+export default Footer
